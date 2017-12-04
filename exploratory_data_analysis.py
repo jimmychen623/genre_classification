3ec9a7fe92661dda63b460287649c709aeed7497
Jimmy Chen <jzc8@cornell.edu>
2017-12-03 20:15:16 -0500

    Finish SVM the best results are linear with 0.03 with 0.695 and rbf with 0.3 with 0.71 accuracy

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -7,6 +7,7 @@ from sklearn import svm
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn import preprocessing
 
@@ -66,45 +67,103 @@ def create_train_validation_test(df, genre):
 
 X_train, y_train, X_validation, y_validation, X_test, y_test = create_train_validation_test(dataframe, 'Jazz')
 
-param_grid = [
-  {'C': [0.003, 0.01, 0.03], 'kernel': ['rbf']}
-  #{'C': [0.01, 0.1, 1, 10, 100, 1000], 'gamma': [0.1, 0.01, 0.001, 0.0001], 'kernel': ['rbf']},
- ]
+# param_grid = [
+#   {'C': [0.03, 0.1, 0.3, 1, 2], 'kernel': ['linear']},
+#   {'C': [0.03, 0.1, 0.3, 1, 2], 'gamma': [3.0, 1.0, 0.3, 0.1, 0.03], 'kernel': ['rbf']},
+#  ]
+#
+# scores = ['accuracy', 'precision_micro', 'recall_micro', 'f1_micro']
+# for score in scores:
+#     print("# Tuning hyper-parameters for %s" % score)
+#     print()
+#
+#     clf = GridSearchCV(SVC(), param_grid, cv=5,
+#                        scoring=score)
+#     print('gjg')
+#     labels = y_train['genre']
+#     clf.fit(X_train, labels)
+#
+#     print("Best parameters set found on development set:")
+#     print()
+#     print(clf.best_params_)
+#     print()
+#     print("Grid scores on development set:")
+#     print()
+#     means = clf.cv_results_['mean_test_score']
+#     stds = clf.cv_results_['std_test_score']
+#     for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+#         print("%0.3f (+/-%0.03f) for %r"
+#               % (mean, std * 2, params))
+#     print()
+#
+#     print("Detailed classification report:")
+#     print()
+#     print("The model is trained on the full development set.")
+#     print("The scores are computed on the full evaluation set.")
+#     print()
+#     y_true, y_pred = y_test, clf.predict(X_test)
+#     print(classification_report(y_true, y_pred))
+#     print()
+#     print(accuracy_score(y_true, y_pred))
 
-scores = ['precision_micro', 'recall_micro']
-for score in scores:
-    print("# Tuning hyper-parameters for %s" % score)
-    print()
+print("rbf 0.1")
+clf = svm.SVC(kernel='rbf', C=0.1, gamma = 0.3).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
 
-    clf = GridSearchCV(SVC(), param_grid, cv=5,
-                       scoring=score)
-    print('gjg')
-    labels = y_train['genre']
-    clf.fit(X_train, labels)
+print("rbf 0.3")
+clf = svm.SVC(kernel='rbf', C=0.3, gamma = 0.3).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
 
-    print("Best parameters set found on development set:")
-    print()
-    print(clf.best_params_)
-    print()
-    print("Grid scores on development set:")
-    print()
-    means = clf.cv_results_['mean_test_score']
-    stds = clf.cv_results_['std_test_score']
-    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-        print("%0.3f (+/-%0.03f) for %r"
-              % (mean, std * 2, params))
-    print()
+print("rbf 1")
+clf = svm.SVC(kernel='rbf', C=1, gamma = 0.3).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
 
-    print("Detailed classification report:")
-    print()
-    print("The model is trained on the full development set.")
-    print("The scores are computed on the full evaluation set.")
-    print()
-    y_true, y_pred = y_test, clf.predict(X_test)
-    print(classification_report(y_true, y_pred))
-    print()
+print("rbf 3")
+clf = svm.SVC(kernel='rbf', C=3, gamma = 0.3).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
 
+print("rbf 10")
+clf = svm.SVC(kernel='rbf', C=10, gamma = 0.3).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
 
-clf = svm.SVC(kernel='rbf', C=1).fit(X_train, y_train)
+print("\n")
+
+print("linear 0.01")
+clf = svm.SVC(kernel='linear', C=.01).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
+
+print("linear 0.03")
+clf = svm.SVC(kernel='linear', C=0.03).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
+
+print("linear 0.1")
+clf = svm.SVC(kernel='linear', C=0.1).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
+
+print("linear 0.3")
+clf = svm.SVC(kernel='linear', C=0.3).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
+
+print("linear 1")
+clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
+
+print("linear 3")
+clf = svm.SVC(kernel='linear', C=3).fit(X_train, y_train)
+scores = clf.score(X_test, y_test)
+print(scores)
+
+print("linear 10")
+clf = svm.SVC(kernel='linear', C=10).fit(X_train, y_train)
 scores = clf.score(X_test, y_test)
 print(scores)
